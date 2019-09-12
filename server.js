28c9d796fae1b378bc5a052a40b75d9617631fbf
Ben Mandakh <benmandakh@gmail.com>
2019-09-12 13:36:10 -0700

    Added Angular App

--- server.js
+++ server.js
@@ -1,61 +1,40 @@
-////////// EXPRESS ///////////////////////////////////
-const express = require("express");
-const app = express();
+//Imports
 
-////////// MONGOOSE //////////////////////////////////
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/apiDB', {useNewUrlParser: true});
-const PeopleSchema = new mongoose.Schema({
- name: {type: String, required:true, minlength:2},
-}, {timestamps:true});
-const People = mongoose.model('People', PeopleSchema);
+var express = require("express");
 
-////////// CONFIGURE  ////////////////////////////////
-const bodyParser = require("body-parser");
+var app = express();
+
+var bodyParser = require("body-parser");
+
+var mongoose = require("mongoose");
+
+
+
+//Config
+
 app.use(bodyParser.json());
-mongoose.set('useUnifiedTopology', true);
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-
-///////// EXPRESS CONNECTION PORT ////////////////////
-app.listen(8000, () => console.log("listening on port 8000"));
-
-//////// INDEX GET ROUTE /////////////////////////////
-app.get('/', (req, res) => {
-    People.find()
-        .then(people => res.json(people))
-        .catch(err => res.json(err));
-});
-//////// ADD GET ROUTE   /////////////////////////////
-app.post('/new/:name', (req, res) => {
-    const people = new People();
-    people.name = req.params.name;
-    people.save()
-        .then(() => res.redirect('/'))
-        .catch(err => res.json(err));
-});
-//////// REMOVE GET ROUTE ////////////////////////////
-app.delete('/remove/:name', (req, res) => {
-    People.remove({ name:req.params.name}, function(err, person){
-        if(err){
-            res.json(err);
-        }else{
-            res.josn({removed:true});
-        }
-    })
-});
-//////// DETAILS GET ROUTE ///////////////////////////
-app.get('/:name', (req, res) => {
-    People.findOne({ name:req.params.name}, function(err, person){
-        if(err){
-            res.json(err);
-        }else{
-            res.josn(person);
-        }
-    })
-});
+app.use(express.static( __dirname + '/public/dist/public' ));
 
 
 
+//Datebase
+
+mongoose.connect("mongodb://localhost/1955_API")
+
+require("./server/config/mongoose.js");
 
 
+
+//Routes
+
+require("./server/config/routes.js")(app);
+
+
+
+//Port
+
+app.listen(8000, function(){
+
+    console.log("Listening on port: 8000");
+
+})
---
+++ server/config/routes.js
@@ -0,0 +1,8 @@
+var peoples = require("../controllers/peoples");
+module.exports = function(app){
+    app.get("/", peoples.index)
+    app.get("/show", peoples.index)
+    app.get("/new/:name", peoples.addPerson)
+    app.get("/remove/:name", peoples.removePerson)
+    app.get("/:name", peoples.details)
+}
